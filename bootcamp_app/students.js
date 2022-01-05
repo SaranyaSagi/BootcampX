d383fdb3beab1ba620fb88961ce4b9fa29b0b3b5
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-05 17:09:15 -0500

    protected from sql injection bby using  and array for reading outside data

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,7 +7,40 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-//Get students table
+//sql injection protected
+const queryString = `SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+const cohortName = process.argv[2]
+const limit = process.argv[3] || 5
+const values = [`%${cohortName}%`, limit]
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+});
+
+//              Not sql injection safe
+//    Query parameters so typing node students.js FEB 2 gives you 2 rows from feb cohort students
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// });
+
+//                Get students table
 // pool.query(`
 // SELECT id, name, cohort_id
 // FROM students
@@ -30,18 +63,6 @@ const pool = new Pool({
 //   })
 // });
 
-// Query parameters so typing node students.js FEB 2 gives you 2 rows from feb cohort students
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-});
+
 
 
